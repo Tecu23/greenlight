f49c2841a816201b6b4a25e4097aa2ed58a65a6a
tecu23 <andrei.teculescu@gmail.com>
2024-03-04 14:21:37 +0200

    process json requests

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,16 +1,46 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
+// ErrInvalidRuntimeFormat is a custom error
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
+
+// Runtime type
 type Runtime int32
 
+// MarshalJSON encodes json
 func (r Runtime) MarshalJSON() ([]byte, error) {
 	jsonValue := fmt.Sprintf("%d mins", r)
 
 	quotedJSONValue := strconv.Quote(jsonValue)
 
 	return []byte(quotedJSONValue), nil
+}
+
+// UnmarshalJSON decodes json
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	parts := strings.Split(unquotedJSONValue, " ")
+
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	*r = Runtime(i)
+
+	return nil
 }
