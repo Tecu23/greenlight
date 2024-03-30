99811aabda45bdd368e35d7c4c29f1e2573a1da6
tecu23 <andrei.teculescu@gmail.com>
2024-03-30 12:23:41 +0200

    cors + simple and preflight testing

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"flag"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -37,6 +38,10 @@ type config struct {
 		username string
 		password string
 		sender   string
+	}
+
+	cors struct {
+		trustedOrigins []string
 	}
 }
 
@@ -82,6 +87,15 @@ func main() {
 		"smtp-sender",
 		"Greenlight <no-reply@greenlight.alexedwards.net>",
 		"SMTP sender",
+	)
+
+	flag.Func(
+		"cors-trusted-origins",
+		"Trusted CORS origins (space separated)",
+		func(val string) error {
+			cfg.cors.trustedOrigins = strings.Fields(val)
+			return nil
+		},
 	)
 
 	flag.Parse()
